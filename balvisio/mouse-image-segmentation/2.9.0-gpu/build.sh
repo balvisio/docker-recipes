594e377192692f2d6ff6b6af9d6df4586d426d62
Bruno Alvisio <bruno.alvisio@gmail.com>
2022-11-10 17:55:31 -0600

    New versioning mechanism

--- balvisio/mouse-image-segmentation/2.9.0-gpu/build.sh
+++ balvisio/mouse-image-segmentation/2.9.0-gpu/build.sh
@@ -2,5 +2,5 @@
 
 image_name=mouse-image-segmentation:$(grep FROM Dockerfile | awk -F: '{ print $2 }')
 
-
-docker build . -t balvisio/${image_name}-custom-mlflow
+subversion=$(cat version)
+docker build . -t balvisio/${image_name}-${subversion}
--- balvisio/mouse-image-segmentation/2.9.0/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-image_name=mouse-image-segmentation:$(grep FROM Dockerfile | awk -F: '{ print $2 }')
-
-
-docker build . -t balvisio/${image_name}
