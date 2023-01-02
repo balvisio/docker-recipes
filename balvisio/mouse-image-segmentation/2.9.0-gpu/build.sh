daed657ef44c4ad17a975fa9baf9900a1bb3b0ea
Bruno Alvisio <bruno.alvisio@gmail.com>
2023-01-02 08:14:28 -0600

    Added version 1 of mouse-image-segmentation image

--- balvisio/mouse-image-segmentation/2.9.0-gpu/build.sh
+++ balvisio/mouse-image-segmentation/2.9.0-gpu/build.sh
@@ -2,5 +2,5 @@
 
 image_name=mouse-image-segmentation:$(grep FROM Dockerfile | awk -F: '{ print $2 }')
 
-subversion=$(cat version)
+subversion=$(cat subversion)
 docker build . -t balvisio/${image_name}-${subversion}
